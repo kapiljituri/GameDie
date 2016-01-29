60e259c7d41d9a4821507d1e2c31c837313d3213
Kapil Jituri <kapiljituri@gmail.com>
2016-01-29 10:49:32 -0800

    Implemented roll() function.

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <cstdlib>
+#include <stdlib.h>
+#include <ctime>
 
+using namespace std;
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+  srand(time(NULL));
 }
 
 //generate a random number between 1-6 (inclusive) and display it
 void GameDie::roll()
 {
+  cout << (rand()%6)+1 << endl;
 }
